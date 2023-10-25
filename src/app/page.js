759a1559f8046c83f6558bc10a3216c1d7f1b509
Ali Raza <aliraza@192.168.10.15>
2023-10-25 14:34:12 -0700

    upload

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: "sbtc loan",
+  description: "A decentralized Bitcoin lending application",
+};
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="mt-8 text-4xl text-center">sbtcdeposit</h1>
+      <p className="mt-4 text-center">
+        Decentralized lending and borrowing with sBTC.
+      </p>
+    </>
+  );
+}
